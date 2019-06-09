7f1df5f3de7705a523a514cfdd6582f96cb6ee05
Etienne Mustow <etienne.o.mustow@gmail.com>
2019-06-09 18:09:41 +0100

    Mock data extracted into constants

---
+++ spec/helpers/mock_data.rb
@@ -0,0 +1,25 @@
+MOCK_PURCHASES_DATA = [
+  {"user_id"=>"FFWN-1CKR-X4WU-Q44M", "item"=>"Awesome Marble Clock", "spend"=>"69.44"},
+  {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Synergistic Concrete Pants", "spend"=>"9.87"},
+  {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Synergistic Concrete Pants", "spend"=>"76.06"},
+  {"user_id"=>"BMCS-1VS1-39KR-7OUM", "item"=>"Durable Wool Shoes", "spend"=>"66.35"},
+  {"user_id"=>"T8VE-2OW8-A7SF-JZOB", "item"=>"Durable Wool Shoes", "spend"=>"31.3"},
+  {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Mediocre Wooden Keyboard", "spend"=>"76.0"},
+  {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Gorgeous Aluminum Plate", "spend"=>"30.84"},
+  {"user_id"=>"EIGM-YGNY-BE29-UXXY", "item"=>"Durable Aluminum Bag", "spend"=>"5.2"},
+  {"user_id"=>"T8VE-2OW8-A7SF-JZOB", "item"=>"Rustic Steel Wallet", "spend"=>"80.34"},
+  {"user_id"=>"PJ69-9VN5-D5NA-IEB4", "item"=>"Synergistic Bronze Bottle", "spend"=>"35.55"},
+  {"user_id"=>"NGE1-SZL3-4WUU-NWD9", "item"=>"Incredible Linen Pants", "spend"=>"30.78"},
+  {"user_id"=>"7IE1-PDJ4-IPH6-8K3L", "item"=>"Gorgeous Steel Computer", "spend"=>"17.75"},
+  {"user_id"=>"S27G-8UMJ-LDSL-UOPN", "item"=>"Gorgeous Silk Table", "spend"=>"53.43"},
+  {"user_id"=>"EIGM-YGNY-BE29-UXXY", "item"=>"Gorgeous Rubber Shoes", "spend"=>"71.43"},
+  {"user_id"=>"T8VE-2OW8-A7SF-JZOB", "item"=>"Durable Wool Shoes", "spend"=>"31.3"},
+  {"user_id"=>"S27G-8UMJ-LDSL-UOPN", "item"=>"Durable Wool Shoes", "spend"=>"31.3"}
+]
+
+MOCK_USERS_DATA = [
+  {"id" => "KZHR-1H35-2IH8-JXYN","first_name"=> "Quincy","last_name"=> "Schimmel","phone"=> "186.301.6921 x948","email"=> "schimmel_quincy@ernser.io"},
+  {"id"=> "S27G-8UMJ-LDSL-UOPN","first_name"=> "Henry","last_name"=> "Terry","phone"=> "636-387-6074 x690","email"=> "terry_henry@doyle.io"},
+  {"id"=> "HTNF-7RJY-YFCU-GUN2","first_name"=> "Tierra","last_name"=> "Langosh","phone"=> "570-113-3234 x7287","email"=> "langosh.tierra@erdman.co"},
+  {"id"=> "ZZLB-4HCN-OA3N-LGWB","first_name"=> "Jack","last_name"=> "Lakin","phone"=> "(620) 104-0175","email"=> "jack_lakin@rodriguezschuppe.io"}
+]
